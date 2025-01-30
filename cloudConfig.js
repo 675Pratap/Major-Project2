b20f81fd4e8fa49717749fab5df9825141417b38
675Pratap <161279488+675Pratap@users.noreply.github.com>
2025-01-30 13:16:03 +0530

    Update cloudConfig.js
    cloud config details added

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+CLOUD_NAME=drz37msvd
+CLOUD_API_KEY=538592972797747
+CLOUD_API_SECRET=17DOSppo7-OTh842kdDsVCt8EWU
 });
 
 const storage = new CloudinaryStorage({
